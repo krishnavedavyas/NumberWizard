0f2811e72e043f7933069a0bf9f90e756bf1b6ee
krishnavedavyas <64304953+krishnavedavyas@users.noreply.github.com>
2020-05-31 11:52:23 +0530

    NumberWizard Uploded

---
+++ Number Wizard/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+   [SerializeField] int min;
+    [SerializeField]int max;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    void Start()
+    {
+        StartGame();
+       
+    }
+    void StartGame()
+    {
+      
+        guess = (min + max) / 2;
+        guessText.text = guess.ToString();
+        max++;
+    }
+    public void OnPressingHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+    public void OnPressingLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        guessText.text = guess.ToString();
+    }
+}
---
+++ Number Wizard/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void loadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void loadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+ public void qUIT()
+    {
+        Application.Quit();
+    }
+}
